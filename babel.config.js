f3ee01d5a8563311908ee83080d4a8c134414db0
aku <a1259154164@163.com>
2025-08-10 16:40:11 +0800

    feat: add

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: aku
+ * @Date: 2025-07-22 00:30:12
+ * @LastEditors: aku
+ * @LastEditTime: 2025-07-24 00:24:24
+ * @FilePath: \my-vue2-project\babel.config.js
+ * @Description: 
+ */
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  plugins: ["transform-remove-strict-mode"]
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
+/*
+ * @Author: aku
+ * @Date: 2025-07-22 00:30:12
+ * @LastEditors: aku
+ * @LastEditTime: 2025-07-29 00:42:39
+ * @FilePath: \my-vue2-project\src\main.js
+ * @Description: 
+ */
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import router from './router'
+
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI);
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
